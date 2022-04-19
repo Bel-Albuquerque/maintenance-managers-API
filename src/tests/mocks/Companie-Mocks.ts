c829393870d7675b3729f8ca81bea6e762ed87ab
bel <mariaisabelalbuquerque@gmail.com>
2022-04-19 20:10:44 -0300

    feat: implementa testes para a rota companie

---
+++ src/tests/mocks/Companie-Mocks.ts
@@ -0,0 +1,54 @@
+export const createCompanie = {
+  CNPJ: "56960678912345",
+  name: "Vaquinha Feliz",
+  password: "12345678",
+  users: []
+}
+
+export const createdCompanie = {
+  CNPJ: "56960678912345",
+  name: "Vaquinha Feliz",
+  password: "12345678",
+  users: [],
+}
+
+export const allCompanies = [
+  {
+    CNPJ: "12345678912345",
+    name: "Ziggy Stardust",
+    password: "08011947",
+    users: [],
+  },
+  {
+    CNPJ: "56745678912345",
+    name: "Freios e Contrapesos",
+    password: "14071789",
+    users: []
+  },
+  {
+    CNPJ: "56960678912345",
+    name: "Vaquinha Feliz",
+    password: "12345678",
+    users: []
+  },
+  {
+    CNPJ: "56960689852345",
+    name: "Dayse Embarcações",
+    password: "17hgxgg8",
+    users: []
+  }
+]
+
+export const editedObj = {
+  CNPJ: "12345678912345",
+  name: "Fabrica suco Feliz",
+  password: "12345678",
+  users: [],
+}
+
+export const findOneCompanie = {
+  CNPJ: "12345678912345",
+  name: "Ziggy Stadust",
+  password: "08011947",
+  users: [],
+}
