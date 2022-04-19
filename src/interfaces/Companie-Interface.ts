4888751cd3baa807b7f55e2b98157c4ec8c05727
bel <mariaisabelalbuquerque@gmail.com>
2022-04-19 20:22:24 -0300

    feat: implementa extensão de interface

--- src/interfaces/Companie-Interface.ts
+++ src/interfaces/Companie-Interface.ts
@@ -1,10 +1,15 @@
 import { z } from 'zod';
 
-export const CompaniesInterface = z.object({
+export const CompanieInterface = z.object({
   CNPJ: z.string().length(14),
   name: z.string().max(200),
   password: z.string().min(6),
   users: z.array(z.object({ userId: z.string() })).optional(),
 });
 
-export type Companie = z.infer<typeof CompaniesInterface>;
+export type Companie = z.infer<typeof CompanieInterface>;
+
+export interface CompanieAuthorization extends Companie {
+  Authorization: string,
+}
+
